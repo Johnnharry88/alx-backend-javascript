6cb04936bf030df1d1985bb1e8944f51e5b1f338
Johnharry88 <harryjohnwhye@gmail.com>
2024-06-26 12:10:36 +0100

    Backend-Java

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpUserPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
-    const res = [];
-    values.ForEach((element) => {
-      if (element.status === `fulflled`){
-        res.push({ status: element.status, value: element.value });
-      } else {
-	res.push({ status: element.status, value: `${element.reason}` });
-      }
-    });
-    return res;
-  });
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => (
+      result.map((o) => ({
+        status: o.status,
+        value: o.status === `fulfilled` ? o.value : String(o.reason),
+      }))
+    ));
 }
