f4b6d2415ef03ea1bce9e2ec24738ec57346c6e1
Johnharry88 <harryjohnwhye@gmail.com>
2024-06-27 22:02:23 +0100

    Java class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a stiing');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (!Number.isInteger(length)) throw new TypeError('Length must be a number');
     if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+
     this._name = name;
     this._length = length;
     this._students = students;
