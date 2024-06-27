1dc09dd5731060bd95f7d54e86d074f88568fcd6
Johnharry88 <harryjohnwhye@gmail.com>
2024-06-27 21:54:34 +0100

    Java class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a stiing');
+    if (!Number.isInteger(length)) throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('Name must be a string');
+    this._name = val
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('Length must be a number')
+    this._length = val;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
+  }
+}
+  
