d98bdae1fbd1ef597a8b571d498c7aadb3dfa156
Johnharry88 <harryjohnwhye@gmail.com>
2024-06-27 21:31:13 +0100

    Java classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,6 @@ export default function initializeRoom() {
   const c1 = new ClassRoom(19);
   const c2 = new ClassRoom(20);
   const c3 = new ClassRoom(34);
-  const nList = [c1, c2,c3];
+  const nList = [c1, c2, c3];
   return (nList);
 }
