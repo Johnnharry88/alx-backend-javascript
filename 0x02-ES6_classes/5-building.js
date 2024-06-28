bb74580e8ffa3fb46556ec2316431a29c1811cc0
Johnharry88 <harryjohnwhye@gmail.com>
2024-06-28 14:13:08 +0100

    Backend-Java

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building && this.evacuationWarningMessage !== 'function') {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
