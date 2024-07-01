f6e864dccdc205e02163dcb1f457f18a2d33aacf
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-01 08:52:59 +0100

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const l1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const l2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const l3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [l1, l2, l3];
+}
