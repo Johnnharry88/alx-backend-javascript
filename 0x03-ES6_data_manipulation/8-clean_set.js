61404a7cf1edecdcad74db4a53031a07646c4a1b
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-02 16:23:37 +0100

    Data Mani

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,13 @@ export default function clearSet(set, startString) {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
-  
+
   const results = [];
 
   for (const alx of set.values()) {
     if (typeof alx === 'string' && alx.startsWith(startString)) {
       const alxRes = alx.substring(startString.length);
-      
+
       if (alxRes && alxRes !== alx) {
         results.push(alxRes);
       }
