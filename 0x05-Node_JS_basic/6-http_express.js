4c9ab1b82dabdc80d0e931647378b3d7d88fdac8
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-23 14:13:13 +0100

    Node Basics

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const exp = require('express');
+
+const app = exp();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {});
+
+module.exports = app;
