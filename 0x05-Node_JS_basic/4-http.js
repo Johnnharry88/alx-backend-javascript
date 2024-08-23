d6693d704653b8e85e4685fc5dff6b9ba81b237c
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-23 07:45:24 +0100

    Node_Basics

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const web = require('http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = web.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app
